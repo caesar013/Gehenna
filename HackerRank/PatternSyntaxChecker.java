0412ab90d04abf05f9ff9897b8590d9ce6985da0
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-06 06:58:00 +0700

    hackerrank syntax checker

---
+++ HackerRank/PatternSyntaxChecker.java
@@ -0,0 +1,23 @@
+package HackerRank;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+public class PatternSyntaxChecker{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        s.nextLine();
+        while (x-- > 0) {
+            String str = s.nextLine();
+            try {
+                Pattern p = Pattern.compile(str);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException a) {
+                System.out.println("Invalid");
+            }
+        }
+        s.close();
+    }
+}
