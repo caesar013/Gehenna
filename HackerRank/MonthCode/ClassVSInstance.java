dc7cadfb41328b194c4c8df04139c7075cf6f0fb
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-07 23:14:53 +0700

    some deletion and adding

--- HackerRank/MonthCode/ClassVSInstance.java
+++ HackerRank/MonthCode/ClassVSInstance.java
@@ -1,8 +1,17 @@
 package HackerRank.MonthCode;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ClassVSInstance {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(bf.readLine().trim());
         
+        while (i-- > 0) {
+            
+        }
     }
 }
 
@@ -15,6 +24,20 @@ class Person{
         } else {
             age = 0;
             System.out.println("Age is not valid, setting age to 0.");
+        }
+    }
+
+    public void yearPasses(){
+        age++;
+    }
+
+    public void amIOld(){
+        if (age < 13) {
+            System.out.println("You are young.");
+        } else if (age >= 13 && age <18){
+            System.out.println("You are a teenager.");
+        } else {
+            System.out.println("You are old.");
         }
     }
 }
