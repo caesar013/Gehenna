0c4b4ea613452ff805e54b1aa6168b0b12d0bcea
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-09 19:06:12 +0700

    Hackerrank Loops

--- HackerRank/MonthCode/Loops.java
+++ HackerRank/MonthCode/Loops.java
@@ -7,5 +7,14 @@ import java.io.InputStreamReader;
 public class Loops {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int i = Integer.parseInt(bf.readLine().trim());
+        if (i < 2 || i > 20) {
+            bf.close();
+            System.exit(0);
+        }
+        for (int j = 1; j < 11; j++) {
+            System.out.printf("%d x %d = %d\n", i,j,(i*j));
+        }
+        bf.close();
     }
 }
