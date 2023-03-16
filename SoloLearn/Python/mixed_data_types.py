73fd0f38eba9ba57407e64f7466406786481df50
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-16 20:43:20 +0700

    Sololearn Mixed data

---
+++ SoloLearn/Python/mixed_data_types.py
@@ -0,0 +1,42 @@
+''' 
+A line of code is called a statement. We use statements to give computer instructions. You can include as many lines or statements as you need in your code. When you give the computer many statements, the instructions will be executed line by line, from top to bottom.
+'''
+
+print("Name")
+print("Caesar")
+print("Balance :")
+print("Unlimited")
+
+'''
+The print command can take multiple values to output on the same line. This can be done by separating them with commas.
+'''
+
+print("Balance :", 100_000_000)
+
+# By default, the values will be separated by spaces.
+
+'''
+When printing multiple outputs, you can customize the separator using the sep parameter.
+'''
+
+print("Iron","Man",sep='-')
+
+# The sep parameter should always be defined after the values in the print command.
+
+'''
+You can add comments to your code to add annotations and explanations. Comments make the code easier to understand for humans. Computers ignore comments.
+
+A comment in Python starts with #:
+'''
+
+'''
+Note :
+Comments don't affect how the code runs or the output it produces. Comments usually explain what the code does.
+'''
+
+'''
+Summary :
+1. You can add many statements (or lines of code) to your programs
+2. You can print multiple pieces of data on the same line, by separating them with commas or a custom separator in the print statement.
+3. You can use comments to explain what the code does
+'''
