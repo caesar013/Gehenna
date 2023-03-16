15bb6c6b6d574b8cf13d93aa9a3aad6e4f893241
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-16 20:28:44 +0700

    Sololearn String

---
+++ SoloLearn/Python/StringDataType.py
@@ -0,0 +1,30 @@
+'''
+We use quotation marks to tell Python that we are working with a piece of text data or string. Choosing double " or single ' quotation marks doesn't affect how the string behaves in any way.
+'''
+
+print("I am a String")
+
+'''
+In Python, some arithmetic operators can also be applied to strings.
+
+We can join two strings together, also known as concatenation
+'''
+
+print("Hello " + 'World')
+
+'''
+Anything in quotes is a string, even numbers. Numbers in quotes will be treated as a piece of text data and you won’t be able to perform arithmetic operations with them.
+'''
+
+print("26" + "9")
+
+'''
+Note : in the code above, 28 and 6 are strings, not numbers. The computer understood that we want these values to behave as pieces of text.
+'''
+
+'''
+Summary :
+1. A string is the data type for text.
+2. Strings require either single or double quotation marks to be understood by computers.
+3. You can join two or more strings together using concatenation (+).
+'''
