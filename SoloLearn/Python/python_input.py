025b87733d4dae067c29eec336a98c96a0bbffd1
Fikril H Ramadhani <fikrilha099@gmail.com>
2023-03-16 21:00:54 +0700

    Sololearn input

---
+++ SoloLearn/Python/python_input.py
@@ -0,0 +1,22 @@
+# The input() command asks the user for input and returns what they enter as a STRING
+
+x = input()
+print(x)
+
+# Even if the user enters a number as input, it's processed as a string
+
+number = input()
+print("You entered: " + number)
+
+# The input() command needs to be followed by parentheses.
+
+name = input()
+print("Welcome, " + name)
+
+'''
+ 💡 The input() command is used to make your programs interact with the user.
+
+ 💡 The input() command always returns a string even if the user inputs a number.
+
+ 💡 You can customize the message you want the user to see when an input is needed 
+ '''
