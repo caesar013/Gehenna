5e69282a873d91fb4b63a705bd0d2bc97e0e252e
cαεsαɾ <91421259+caesar013@users.noreply.github.com>
2024-05-28 16:22:09 +0700

    add : String to Integer problem

---
+++ HackerRank/Java/MonthCode/StringtoInteger.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class StringtoInteger {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = bufferedReader.readLine();
+
+        bufferedReader.close();
+
+        try {
+            System.out.println(Integer.parseInt(S));
+        } catch (Exception e) {
+            // TODO: handle exception
+            System.out.println("Bad String");
+        }
+    }
+}
